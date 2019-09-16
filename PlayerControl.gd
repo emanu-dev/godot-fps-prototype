30fbbd4c78463b0c41e6fe4974ee904ddc33a305
Emanuel Prado <emanuel.prado.dev@gmail.com>
2019-09-16 20:58:42 -0300

    Add blend to bobbing animation

--- PlayerControl.gd
+++ PlayerControl.gd
@@ -7,8 +7,12 @@ onready var anim_player = $AnimationPlayer
 onready var bobbing_player = $BobbingPlayer
 onready var raycast = $RayCast
 onready var camera = $Camera
+onready var state_machine = $AnimationTree.get("parameters/playback")
+
+var move_vec
 
 func _ready():
+	state_machine.start('still')
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Get mouse stuck on center
 	yield(get_tree(), "idle_frame") # Wait one frame
 	get_tree().call_group("zombies", "set_player", self) # set the player on all enemies
@@ -19,13 +23,23 @@ func _input(event):
 		rotation_degrees.y -= MOUSE_SENS * event.relative.x
 		
 func _process(delta):
+
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
 	if Input.is_action_pressed("restart"):
 		kill()
+
+	
+	if move_vec != Vector3(0,0,0) && !anim_player.is_playing():
+		print("hooray")
+		state_machine.travel("walking")
+	else:
+		state_machine.travel("still")		
+		
 		
 func _physics_process(delta):
-	var move_vec = Vector3()
+	move_vec = Vector3()	
+	
 	if Input.is_action_pressed("move_forwards"):
 		move_vec.z -= 1
 	if Input.is_action_pressed("move_backwards"):
@@ -38,11 +52,6 @@ func _physics_process(delta):
 	move_vec = move_vec.normalized()
 	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
 	move_and_collide(move_vec * MOVE_SPEED * delta)
-	
-	if move_vec != Vector3(0,0,0) && !anim_player.is_playing():
-		bobbing_player.play("walking")
-	else:
-		bobbing_player.play("still")
 	
 	if Input.is_action_pressed("shoot") and !anim_player.is_playing():
 		anim_player.play("shoot")
